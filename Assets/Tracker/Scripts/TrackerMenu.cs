9228889f6b76102a07c772df33905fa252094503
BlazinZzetti <blazinzzetti@gmail.com>
2019-06-20 23:45:28 -0700

    Fixed sprite material references. Updated Unity version.  Now buildable.

--- Assets/Tracker/Scripts/TrackerMenu.cs
+++ Assets/Tracker/Scripts/TrackerMenu.cs
@@ -12,65 +12,65 @@ public class TrackerMenu : MonoBehaviour
 
     public TrackerManager Manager;
 
-    [DllImport("user32.dll")]
-    private static extern void OpenFileDialog();
+ //   [DllImport("user32.dll")]
+ //   private static extern void OpenFileDialog();
 
-    [DllImport("user32.dll")]
-    private static extern void SaveFileDialog();
+ //   [DllImport("user32.dll")]
+ //   private static extern void SaveFileDialog();
 
-    // Use this for initialization
-    void Start ()
-    {
-        LoadFileLocationButton.onClick.AddListener(onLoadFileLocationButtonPressed);
-        SaveFileButton.onClick.AddListener(onSaveFileButtonPressed);
-        ReturnToMenuButton.onClick.AddListener(onReturnToMenuButtonPressed);
-	}
+ //   // Use this for initialization
+ //   void Start ()
+ //   {
+ //       LoadFileLocationButton.onClick.AddListener(onLoadFileLocationButtonPressed);
+ //       SaveFileButton.onClick.AddListener(onSaveFileButtonPressed);
+ //       ReturnToMenuButton.onClick.AddListener(onReturnToMenuButtonPressed);
+	//}
 
-    void onLoadFileLocationButtonPressed()
-    {
-        System.Windows.Forms.OpenFileDialog ofd = new System.Windows.Forms.OpenFileDialog();
+ //   void onLoadFileLocationButtonPressed()
+ //   {
+ //       System.Windows.Forms.OpenFileDialog ofd = new System.Windows.Forms.OpenFileDialog();
 
-        var result = ofd.ShowDialog();
+ //       var result = ofd.ShowDialog();
 
-        if (result == System.Windows.Forms.DialogResult.OK && File.Exists(ofd.FileName))
-        {
-            // Open the file to read from.
-            using (StreamReader sr = File.OpenText(ofd.FileName))
-            {
-                string s = "";
-                foreach (var path in Manager.PathControl.Paths)
-                {
-                    if ((s = sr.ReadLine()) != null)
-                    {
-                        path.CompletedToggle.isOn = bool.Parse(s);
-                    }
-                }
-            }
-            Manager.storedFilePath = ofd.FileName;
-        }
-    }
+ //       if (result == System.Windows.Forms.DialogResult.OK && File.Exists(ofd.FileName))
+ //       {
+ //           // Open the file to read from.
+ //           using (StreamReader sr = File.OpenText(ofd.FileName))
+ //           {
+ //               string s = "";
+ //               foreach (var path in Manager.PathControl.Paths)
+ //               {
+ //                   if ((s = sr.ReadLine()) != null)
+ //                   {
+ //                       path.CompletedToggle.isOn = bool.Parse(s);
+ //                   }
+ //               }
+ //           }
+ //           Manager.storedFilePath = ofd.FileName;
+ //       }
+ //   }
 
-    void onSaveFileButtonPressed()
-    {
-        System.Windows.Forms.SaveFileDialog sfd = new System.Windows.Forms.SaveFileDialog();
+ //   void onSaveFileButtonPressed()
+ //   {
+ //       System.Windows.Forms.SaveFileDialog sfd = new System.Windows.Forms.SaveFileDialog();
 
-        sfd.DefaultExt = ".txt";
+ //       sfd.DefaultExt = ".txt";
 
-        var result = sfd.ShowDialog();
+ //       var result = sfd.ShowDialog();
 
-        if (result == System.Windows.Forms.DialogResult.OK)
-        {
-            // Create a file to write to.
-            using (StreamWriter sw = File.CreateText(sfd.FileName))
-            {
-                foreach (var path in Manager.PathControl.Paths)
-                {
-                    sw.WriteLine(path.CompletedToggle.isOn);
-                }
-            }
-            Manager.storedFilePath = sfd.FileName;
-        }
-    }
+ //       if (result == System.Windows.Forms.DialogResult.OK)
+ //       {
+ //           // Create a file to write to.
+ //           using (StreamWriter sw = File.CreateText(sfd.FileName))
+ //           {
+ //               foreach (var path in Manager.PathControl.Paths)
+ //               {
+ //                   sw.WriteLine(path.CompletedToggle.isOn);
+ //               }
+ //           }
+ //           Manager.storedFilePath = sfd.FileName;
+ //       }
+ //   }
 
     void onReturnToMenuButtonPressed()
     {
